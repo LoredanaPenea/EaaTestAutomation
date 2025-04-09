1fa5a532509d65774db942432b443bea24a72e18
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2025-04-09 10:28:34 +0300

    add test for local app

--- EaaApplicationTest/UnitTest1.cs
+++ EaaApplicationTest/UnitTest1.cs
@@ -3,7 +3,7 @@ using EaaFramework.Driver;
 using Microsoft.Playwright;
 
 
-namespace PlaywrightDemo
+namespace EaaApplicationTest
 {
     public class Tests: IClassFixture<PlaywrightDriverInitializer>
     {
@@ -33,6 +33,27 @@ namespace PlaywrightDemo
             await page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Log in"}).ClickAsync();
 
             await page.GetByRole(AriaRole.Link, new PageGetByRoleOptions { Name = "Employee List" }).ClickAsync();
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            var page = await _playwrightDriver.Page;
+            await page.GotoAsync("http://localhost:33084/");
+            await page.GetByRole(AriaRole.Link, new() { Name = "Product" }).ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
+            await page.GetByRole(AriaRole.Textbox, new() { Name = "Name" }).ClickAsync();
+            await page.GetByRole(AriaRole.Textbox, new() { Name = "Name" }).FillAsync("UPS");
+            await page.GetByRole(AriaRole.Textbox, new() { Name = "Description" }).ClickAsync();
+            await page.GetByRole(AriaRole.Textbox, new() { Name = "Description" }).FillAsync("Uninterrupted Power Supply backup");
+            await page.Locator("#Price").ClickAsync();
+            await page.Locator("#Price").FillAsync("1570");
+            await page.GetByLabel("ProductType").SelectOptionAsync(new[] { "2" });
+            await page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
+            await page.GetByRole(AriaRole.Row, new() { Name = "68 UPS Uninterrupted Power" }).GetByRole(AriaRole.Link).Nth(1).ClickAsync();
+            await page.GetByText("UPS").ClickAsync();
+            await page.GetByRole(AriaRole.Link, new() { Name = "Back to List" }).ClickAsync();
+
         }
     }
 }
